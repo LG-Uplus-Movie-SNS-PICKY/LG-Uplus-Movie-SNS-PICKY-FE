b8ccf85ca585c5ef0682714475b5352e8c71e34d
JGW-Korea <jgw6372@gmail.com>
2024-11-19 16:06:36 +0900

    feat: Button 컴포넌트 생성 시작

---
+++ src/stories/button/Button.stories.ts
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import { Button } from "./Button";
+
+const meta = {
+  title: "GlobalComponent/Button", // title -> Storybook Directory
+  component: Button, // Storybook props -> 컴포넌트 Props
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// Button 컴포넌트의 스토리북(Storybook) 종류 선언
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: "Button",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: "Button",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: "large",
+    label: "Button",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: "small",
+    label: "Button",
+  },
+};
