6b2e9e578e64708441d307d9cbc918f7959ee69a
JGW-Korea <jgw6372@gmail.com>
2024-11-20 17:12:45 +0900

    feat: vite-plugin-svgr 설치

--- src/stories/header/index.tsx
+++ src/stories/header/index.tsx
@@ -1,11 +1,15 @@
 import styles from "./index.styles";
 
+import Logo from "@assets/icons/logo.svg?react";
+
 export interface HeaderProps {}
 
 export function Header() {
   return (
     <header css={styles.headerContainer()}>
-      <div></div>
+      <div>
+        <Logo />
+      </div>
       <div></div>
     </header>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
     }),
+    svgr(),
   ],
   resolve: {
     alias: {
       "@": "/src", // src 폴더를 @로 별칭 지정
+      "@assets": "/src/assets",
     },
   },
 });
