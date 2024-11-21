98a6ffac8c165cc39e702500b5c93ef39297023f
JGW-Korea <jgw6372@gmail.com>
2024-11-21 15:54:52 +0900

    feat: Movie Item 컴포넌트 레이아웃 정의

--- src/stories/movie-item/index.tsx
+++ src/stories/movie-item/index.tsx
@@ -1,9 +1,53 @@
-import React from "react";
-
 export interface MovieItemProps {
   type: "basic" | "rate" | "all";
+  rate?: number;
+  like?: number;
+  comment?: number;
 }
 
-export function MovieItem({ type }: MovieItemProps): JSX.Element {
-  return <div>MovieItem</div>;
+interface RateComponentProps {
+  count: number;
+}
+
+interface InfoComponentProps {
+  likeCount: number;
+  commentCount: number;
+}
+
+// 평점을 보여주는 컴포넌트
+function RateComponent({ count }: RateComponentProps): JSX.Element {
+  return <></>;
+}
+
+// 좋아요와 댓글의 개수를 보여주는 컴포넌트
+function InfoComponent({
+  likeCount,
+  commentCount,
+}: InfoComponentProps): JSX.Element {
+  return <></>;
+}
+
+export function MovieItem({
+  type,
+  rate,
+  like,
+  comment,
+}: MovieItemProps): JSX.Element {
+  return (
+    <div>
+      {/* 영화 썸네일 이미지 */}
+      <div>
+        <img />
+      </div>
+
+      {/* 영화 제목 */}
+      <span></span>
+
+      {/* Type === "rate" : 별점만 보여주기 */}
+      <div></div>
+
+      {/* Type === "all" 별점 + 댓글 수 + 좋아요 수 보여주기 */}
+      <div></div>
+    </div>
+  );
 }
