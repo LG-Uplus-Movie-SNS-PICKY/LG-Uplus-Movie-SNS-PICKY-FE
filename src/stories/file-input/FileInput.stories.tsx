82398a3a86da96c845e4d52ac253f99a2d055503
JGW-Korea <jgw6372@gmail.com>
2024-11-22 11:12:59 +0900

    feat: File Input Story 정의

---
+++ src/stories/file-input/FileInput.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { FileInput } from "./";
+
+const meta = {
+  title: "GlobalComponent/FileInput",
+  component: FileInput,
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof FileInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// 이미지만 등록할 수 있는 File type Input 태그
+export const BasicFileInput: Story = {
+  args: {},
+};
+
+// 이미지, 영상 파일 모두 등록할 수 있는 File type Input 태그
+export const MediaFileInput: Story = {
+  args: {},
+};
