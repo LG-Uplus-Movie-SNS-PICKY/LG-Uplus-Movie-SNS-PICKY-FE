b37ec3a775f2f95d8aafd8808d5d8edcfb84dff9
JGW-Korea <jgw6372@gmail.com>
2024-11-28 02:26:16 +0900

    feat: Movies 등록 세션 분리 (#22)

---
+++ src/pages/admin/operation/movie/movies/components/register-section/index.tsx
@@ -0,0 +1,5 @@
+function RegistMovieSection() {
+  return <div>RegistMovieSection</div>;
+}
+
+export default RegistMovieSection;
--- src/pages/admin/operation/movie/movies/index.tsx
+++ src/pages/admin/operation/movie/movies/index.tsx
@@ -1,5 +1,14 @@
+import RegistMovieSection from "./components/register-section";
+
 function MoviesOpertionPage() {
-  return <div>MoviesOpertionPage</div>;
+  return (
+    <>
+      {/* 영화 등록 Section */}
+      <RegistMovieSection />
+
+      {/* 등록된 영화 리스트 관리 Section */}
+    </>
+  );
 }
 
 export default MoviesOpertionPage;
