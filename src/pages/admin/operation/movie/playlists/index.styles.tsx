2d01ac8b36c4aee5a2ad57cf2f281b2e76ea143d
JGW-Korea <jgw6372@gmail.com>
2024-11-28 01:27:11 +0900

    feat: Playlist Section 레이아웃 설계 (#22)

--- src/pages/admin/operation/movie/playlists/index.styles.tsx
+++ src/pages/admin/operation/movie/playlists/index.styles.tsx
@@ -14,16 +14,33 @@ export default {
       box-shadow: 0 0 4px rgba(193, 193, 193, 0.4);
 
       & > .container {
+        width: 100%;
         display: flex;
-        justify-content: center;
         align-items: center;
+        justify-content: space-between;
 
         font-size: 14px;
         font-weight: 600;
 
-        & > h3 {
+        & > .section {
+          display: flex;
+        }
+
+        & > .section.total {
+          gap: 4px;
           color: #9d9d9d;
-          margin-right: 8px;
+        }
+
+        & > .section.add button {
+          display: flex;
+          border: 1px solid #aaa;
+
+          padding: 2px 8px;
+          border-radius: 16px;
+          background-color: transparent;
+          align-items: center;
+          gap: 4px;
+          cursor: pointer;
         }
       }
     `;
@@ -44,7 +61,7 @@ export default {
     return css`
       width: 100%;
       background-color: #fff;
-      padding: 8px;
+      padding: 8px 16px;
       border-radius: 4px;
       box-shadow: 0 0 4px rgba(193, 193, 193, 0.4);
 
@@ -130,15 +147,20 @@ export default {
 
       & > button {
         background-color: transparent;
-        border: 1px solid #aaa;
+        border: 1px solid #666;
         padding: 2px 16px;
         border-radius: 16px;
         cursor: pointer;
         outline: none;
-        transition: border-color 0.3s;
+
+        color: #666;
+        font-weight: 600;
+        font-size: 12px;
+        transition: 0.3s;
 
         &:hover {
-          border-color: #666;
+          border-color: #000;
+          color: #000;
         }
       }
     `;
@@ -149,24 +171,38 @@ export default {
       display: flex;
       flex-direction: column;
       gap: 24px;
+      padding: 8px 0;
     `;
   },
 
-  reportMessageBox(): SerializedStyles {
+  swiperContainer(): SerializedStyles {
     return css`
-      padding: 8px;
-      display: flex;
-      flex-direction: column;
-      gap: 12px;
+      width: 100%;
+      height: 100%;
 
-      & > .title {
-        font-size: 20px;
-        font-weight: 600;
+      & > .swiper-wrapper > .swiper-slide {
+        position: relative;
+
+        width: fit-content;
+        overflow: hidden;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
       }
 
-      & > .description {
-        line-height: 1.4;
-        word-break: keep-all;
+      & > .swiper-wrapper > .swiper-slide > img {
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+
+      & > .swiper-wrapper > .swiper-slide.add-circle {
+        width: fit-content;
+        & > svg {
+          cursor: pointer;
+        }
       }
     `;
   },
--- src/pages/admin/operation/movie/playlists/index.tsx
+++ src/pages/admin/operation/movie/playlists/index.tsx
@@ -1,7 +1,65 @@
 import styles from "./index.styles";
 
-import CheckIcon from "@assets/icons/check.svg?react";
-import CancleIcon from "@assets/icons/cancle.svg?react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Mousewheel } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import AddCircleIcon from "@assets/icons/add_circle_small.svg?react";
+import image3 from "@assets/images/dummy/image3.jpeg";
+import { MovieItem } from "@stories/movie-item";
+
+const dummyMoviePlaylistData = [
+  {
+    poster: image3,
+    name: "꼬미 시리즈 1",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 2",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 3",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 4",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 5",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 6",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 7",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 8",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 9",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 10",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 11",
+  },
+  {
+    poster: image3,
+    name: "꼬미 시리즈 12",
+  },
+];
 
 function MoviePlaylistOpertionPage() {
   return (
@@ -10,8 +68,17 @@ function MoviePlaylistOpertionPage() {
       <div css={styles.titleHeaderContainer()}>
         {/* 플레이리스트 개수 */}
         <div className="container">
-          <h3>Total Playlist:</h3>
-          <span>36</span>
+          <div className="section total">
+            <h3>Total Playlist:</h3>
+            <span>36</span>
+          </div>
+
+          <div className="section add">
+            <button>
+              <AddCircleIcon width="16px" height="16px" />
+              <span>추가</span>
+            </button>
+          </div>
         </div>
       </div>
 
@@ -19,6 +86,7 @@ function MoviePlaylistOpertionPage() {
       <div css={styles.playlistContainer()}>
         {/* Playlist Card */}
         <div css={styles.playlistCard()}>
+          {/* Playlist Header */}
           <div css={styles.playlistCardHeader()}>
             {/* PlayList Name + Actions Button */}
             <h3 className="playlist-title">빨간안경 이동진의 픽 Pick</h3>
@@ -28,6 +96,34 @@ function MoviePlaylistOpertionPage() {
               <button>수정</button>
               <button>삭제</button>
             </div>
+          </div>
+
+          {/* Playlist Content */}
+          <div css={styles.reportInfoContainer()}>
+            <Swiper
+              slidesPerView={"auto"}
+              spaceBetween={10}
+              direction={"horizontal"}
+              freeMode={true}
+              modules={[FreeMode, Mousewheel]}
+              mousewheel={{
+                forceToAxis: true,
+              }}
+              css={styles.swiperContainer()}
+            >
+              {dummyMoviePlaylistData.map((data, idx) => {
+                return (
+                  <SwiperSlide key={idx}>
+                    <MovieItem
+                      type="basic"
+                      src={data.poster}
+                      title={data.name}
+                      name={data.name}
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
           </div>
         </div>
       </div>
