d676c0e04719ce5c5a4a50a4f2e750e4d5564c47
JGW-Korea <jgw6372@gmail.com>
2024-11-29 01:24:08 +0900

    feat: 영화 검색창 추가 (#22)

--- src/pages/admin/operation/movie/movies/components/total-movies/index.styles.tsx
+++ src/pages/admin/operation/movie/movies/components/total-movies/index.styles.tsx
@@ -1,1 +1,69 @@
-export default {};
+import { css, SerializedStyles } from "@emotion/react";
+
+export default {
+  titleHeaderContainer(): SerializedStyles {
+    return css`
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+
+      background-color: #fff;
+      padding: 12px 8px;
+      border-radius: 4px;
+
+      box-shadow: 0 0 4px rgba(193, 193, 193, 0.4);
+
+      /* Total, Search Bar Container */
+      & > .container {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        font-size: 14px;
+        font-weight: 600;
+
+        & > .section {
+          display: flex;
+        }
+
+        & > .section.total {
+          gap: 4px;
+          color: #9d9d9d;
+        }
+
+        & > .section.search {
+          display: flex;
+          align-items: center;
+          border: 1px solid #aaa;
+          overflow: hidden;
+          border-radius: 12px;
+          background-color: #f0f0f0;
+
+          & > input[type="text"],
+          & > button {
+            background-color: transparent;
+            border: none;
+            outline: none;
+            padding: 2px 4px 2px 8px;
+          }
+
+          & > input[type="text"] {
+            font-size: 12px;
+          }
+
+          & > button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+        }
+
+        /* Genres Button Section */
+        & > .genres {
+        }
+      }
+    `;
+  },
+};
--- src/pages/admin/operation/movie/movies/components/total-movies/index.tsx
+++ src/pages/admin/operation/movie/movies/components/total-movies/index.tsx
@@ -1,5 +1,35 @@
+import styles from "./index.styles";
+
+import Search from "@assets/icons/search_small.svg?react";
+
 function TotalMoviesSection() {
-  return <div>TotalMoviesSection</div>;
+  return (
+    <>
+      {/* Title */}
+      <div css={styles.titleHeaderContainer()}>
+        {/* 플레이리스트 개수 */}
+        <div className="container">
+          <div className="section total">
+            <h3>Total Movies:</h3>
+            <span>305</span>
+          </div>
+
+          {/* 검색 필터 */}
+          <div className="section search">
+            <input type="text" placeholder="등록한 영화를 검색해주세요." />
+            <button>
+              <Search />
+            </button>
+          </div>
+        </div>
+
+        {/* 장르 필터 */}
+        <div className="gerens">
+          <button>액션</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default TotalMoviesSection;
