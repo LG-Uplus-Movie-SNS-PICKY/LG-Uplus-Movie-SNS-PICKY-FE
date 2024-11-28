ce9fb971f6997d27b829eea81d4cb15f439a2040
JGW-Korea <jgw6372@gmail.com>
2024-11-29 02:39:53 +0900

    feat: Movie Card - OST or Behind Input 생성 (#22)

--- src/pages/admin/operation/movie/movies/components/total-movies/index.styles.tsx
+++ src/pages/admin/operation/movie/movies/components/total-movies/index.styles.tsx
@@ -155,4 +155,68 @@ export default {
       }
     `;
   },
+
+  movieDetailInput(): SerializedStyles {
+    return css`
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 24px;
+
+      & > .input-container {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        font-size: 12px;
+        font-weight: 600;
+        color: #aaa;
+
+        & > label {
+          display: block;
+          width: 60px;
+        }
+
+        & > .input {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          gap: 4px;
+
+          & > .update {
+            display: flex;
+            gap: 8px;
+
+            & > input[type="text"] {
+              flex: 1;
+              background-color: #f6f8fa;
+              border: 1px solid #d1d9e0;
+              border-radius: 6px;
+              outline: none;
+              font-size: 12px;
+              padding: 4px;
+
+              &:focus {
+                border-color: #191919;
+              }
+            }
+
+            & > button {
+              padding: 5px 16px;
+              font-size: 12px;
+              font-weight: 600;
+              border-radius: 6px;
+
+              background-color: #f6f8fa;
+              border: 1px solid #d1d9e0;
+            }
+          }
+
+          & > span {
+            font-size: 10px;
+          }
+        }
+      }
+      margin: 12px 0;
+    `;
+  },
 };
--- src/pages/admin/operation/movie/movies/components/total-movies/index.tsx
+++ src/pages/admin/operation/movie/movies/components/total-movies/index.tsx
@@ -4,7 +4,7 @@ import Search from "@assets/icons/search_small.svg?react";
 
 const movieInfo = {
   title: "이터널 선샤인",
-  original_title: "(Eternal Sunshine of the Spotless Mind)",
+  original_title: "Eternal Sunshine of the Spotless Mind",
   genres: [
     { id: 1, name: "SF" },
     { id: 2, name: "드라마" },
@@ -15,6 +15,8 @@ const movieInfo = {
     "https://image.tmdb.org/t/p/original//6HNRo7VYpvM5x5O921bEF2BG7f4.jpg",
   overview:
     "조엘은 아픈 기억만을 지워준다는 라쿠나사를 찾아가 헤어진 연인 클레멘타인의 기억을 지우기로 결심한다. 기억이 사라져 갈수록 조엘은 사랑이 시작되던 순간, 행복한 기억들, 가슴 속에 각인된 추억들을 지우기 싫어지기만 하는데... 당신을 지우면 이 아픔도 사라질까요? 사랑은 그렇게 다시 기억된다.",
+  ost: "lSQKCla2410PSwe",
+  behind: "OQPS1245xSCasw",
 };
 
 function TotalMoviesSection() {
@@ -91,6 +93,45 @@ function TotalMoviesSection() {
             {/* Actors Slides */}
 
             {/* OST or Behind Input */}
+            <div css={styles.movieDetailInput()}>
+              <div className="input-container">
+                <label htmlFor="ost">OST</label>
+                <div className="input">
+                  <div className="update">
+                    <input
+                      type="text"
+                      id="ost"
+                      placeholder="영화의 OST를 Youtube 재생목록 List Param 값을 작성해주세요."
+                      value={movieInfo.ost}
+                    />
+                    <button>수정</button>
+                  </div>
+
+                  <span>
+                    * Youtube 재생목록의 List Param 값을 기입해주세요.
+                  </span>
+                </div>
+              </div>
+
+              <div className="input-container">
+                <label htmlFor="behind">비하인드</label>
+                <div className="input">
+                  <div className="update">
+                    <input
+                      type="text"
+                      id="behind"
+                      placeholder="영화의 비하인드 Youtube 재생목록 List Param 값을 작성해주세요."
+                      value={movieInfo.behind}
+                    />
+                    <button>수정</button>
+                  </div>
+
+                  <span>
+                    * Youtube 재생목록의 List Param 값을 기입해주세요.
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
