4ef5ec8134471b70b2d02c8a32b8a868d95fd76b
JGW-Korea <jgw6372@gmail.com>
2024-11-29 01:11:41 +0900

    fix: Movies Page Wrapper 추가 (#22)

--- src/pages/admin/operation/movie/movies/index.tsx
+++ src/pages/admin/operation/movie/movies/index.tsx
@@ -1,12 +1,18 @@
 import RegistMovieSection from "./components/register-section";
+import TotalMoviesSection from "./components/total-movies";
+
+import styles from "./index.styles";
 
 function MoviesOpertionPage() {
   return (
     <>
-      {/* 영화 등록 Section */}
-      <RegistMovieSection />
+      <div css={styles.wrapper()}>
+        {/* 영화 등록 Section */}
+        <RegistMovieSection />
 
-      {/* 등록된 영화 리스트 관리 Section */}
+        {/* 등록된 영화 리스트 관리 Section */}
+        <TotalMoviesSection />
+      </div>
     </>
   );
 }
