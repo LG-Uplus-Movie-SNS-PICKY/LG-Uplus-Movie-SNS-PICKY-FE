16dd92a1f10e56ccc11077917f61b2eff977b32e
JGW-Korea <jgw6372@gmail.com>
2024-11-29 17:49:43 +0900

    feat: Login Banner Components 레이아웃 설계 완료 (#46)

--- src/pages/main/components/login-banner/index.tsx
+++ src/pages/main/components/login-banner/index.tsx
@@ -1,5 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./index.styles";
+
 function LoginBanner() {
-  return <div>LoginBanner</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div css={styles.bannerContainer()} onClick={() => navigate("/login")}>
+      <div className="box">
+        <h3>
+          어떤 작품
+          <br />
+          좋아하세요?
+        </h3>
+        <p>
+          <span className="underline">로그인</span>하고 PICKY를 즐겨보세요
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default LoginBanner;
