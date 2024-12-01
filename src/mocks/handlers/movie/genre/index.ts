8bf0f6bfe727026d6d64fb16e22d85d70c2af44a
JGW-Korea <jgw6372@gmail.com>
2024-12-01 19:26:25 +0900

    feat: Movie Genres Mocking Data 구성 (#57)

--- src/mocks/handlers/movie/genre/index.ts
+++ src/mocks/handlers/movie/genre/index.ts
@@ -4,7 +4,20 @@ import { http, HttpHandler, HttpResponse } from "msw";
 const movieHandelrs: HttpHandler[] = [
   // 장르의 종류를 가져오는 모킹 API
   http.get("/api/movie/genre", () => {
-    return HttpResponse.json([{}]);
+    return HttpResponse.json([
+      { genre_id: 1, genre_name: "액션" },
+      { genre_id: 2, genre_name: "로맨스" },
+      { genre_id: 3, genre_name: "뮤지컬" },
+      { genre_id: 4, genre_name: "코미디" },
+      { genre_id: 5, genre_name: "범죄" },
+      { genre_id: 6, genre_name: "애니메이션" },
+      { genre_id: 7, genre_name: "다큐" },
+      { genre_id: 8, genre_name: "호러" },
+      { genre_id: 9, genre_name: "드라마" },
+      { genre_id: 10, genre_name: "스릴러" },
+      { genre_id: 11, genre_name: "SF" },
+      { genre_id: 12, genre_name: "판타지" },
+    ]);
   }),
 ];
 
