a94b0661b52654b361a6f21731cbdd68dd1d7298
JGW-Korea <jgw6372@gmail.com>
2024-12-02 11:38:51 +0900

    feat: Movie Log Slider 공통 컴포넌트 정의 (#53)

--- src/stories/movie-log/MovieLog.stories.tsx
+++ src/stories/movie-log/MovieLog.stories.tsx
@@ -1,6 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { MovieLog } from "./";
 
+import image1 from "@assets/images/dummy/image1.jpeg";
+import image2 from "@assets/images/dummy/image2.jpeg";
+import image3 from "@assets/images/dummy/image3.jpeg";
+
 const meta = {
   title: "GlobalComponent/MovieLog",
   component: MovieLog,
@@ -16,5 +20,23 @@ type Story = StoryObj<typeof meta>;
 
 // 무비로그 일반
 export const BasicFileInput: Story = {
-  args: {},
+  args: {
+    boardContent: [
+      {
+        board_content_id: 1,
+        board_content_url: image1,
+        board_content_type: "Photo",
+      },
+      {
+        board_content_id: 2,
+        board_content_url: image2,
+        board_content_type: "Photo",
+      },
+      {
+        board_content_id: 3,
+        board_content_url: image3,
+        board_content_type: "Photo",
+      },
+    ],
+  },
 };
