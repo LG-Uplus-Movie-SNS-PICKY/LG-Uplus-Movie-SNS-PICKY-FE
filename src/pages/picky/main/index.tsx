de9069a8a9899b01b0357cb368485c15af7f81f7
JGW-Korea <jgw6372@gmail.com>
2024-12-02 16:21:25 +0900

    feat: Genre Button 컴포넌트 import (#63)

--- src/pages/picky/main/index.tsx
+++ src/pages/picky/main/index.tsx
@@ -1,4 +1,10 @@
+import GenreButtons from "@components/genre";
+
 function PickyPage() {
+  // 장르 -> 전역 상태 관리
+
+  // Top 10 영화 -> 리액트 쿼리
+
   return (
     <>
       {/* Movie Backdrop Banner (Best Movie 평점 1등) */}
@@ -6,6 +12,9 @@ function PickyPage() {
       {/* Best Movies Section */}
 
       {/* Genres Tab */}
+      <div>
+        <GenreButtons />
+      </div>
 
       {/* Playlists */}
     </>
