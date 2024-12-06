695baa0210e169547a6784c1e0aafcf3552766dc
JGW-Korea <jgw6372@gmail.com>
2024-12-06 15:56:25 +0900

    feat: Loading Component 추가 (#109)

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,12 @@
+import Lottie from "lottie-react";
+import LoadingLottie from "@assets/lottie/loadingLottie.json";
+
+function Loading() {
+  return (
+    <>
+      <Lottie animationData={LoadingLottie} />
+    </>
+  );
+}
+
+export default Loading;
