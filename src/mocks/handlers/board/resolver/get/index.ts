3af433fd3d2d0495f0fc7c4df246a8c05597e4ed
JGW-Korea <jgw6372@gmail.com>
2024-12-07 00:48:47 +0900

    fix: 빌드 에러 해결 (#111)

--- src/mocks/handlers/board/resolver/get/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MovieLogReadResolver } from "./all-movie-log";
-export { default as UserWriterMovieLogReadResolver } from "./user-movie-log";
-export { default as SeletorMovieLogReadResolver } from "./selector-movie";
