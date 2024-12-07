882bbdfe0d1d69ca3d9d1335d907c179e4a3b4a5
JGW-Korea <jgw6372@gmail.com>
2024-12-07 11:48:10 +0900

    feat: Review GET Mocking Obeject 설계 (#111)

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,11 +1,13 @@
 import { setupWorker } from "msw/browser";
 
-import boardHandlers from "./handlers/board";
+import boardHandlers from "./handlers/board"; // 무비로그 관련 Mocking Object
+import reviewHandler from "./handlers/review"; // 한줄평 관련 Mocking Object
 import movieHandelrs from "./handlers/movie/genre";
 import playlistHandler from "./handlers/movie/playlist";
 
 export const worker = setupWorker(
   ...boardHandlers,
+  ...reviewHandler,
   ...movieHandelrs,
   ...playlistHandler
 );
