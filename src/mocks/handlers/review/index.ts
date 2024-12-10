927e7eca5971e961d5654262230304f979bb9b0f
JGW-Korea <jgw6372@gmail.com>
2024-12-10 19:36:57 +0900

    feat: Review Mockup Data 구성 (#111)

--- src/mocks/handlers/review/index.ts
+++ src/mocks/handlers/review/index.ts
@@ -1,6 +1,8 @@
 import { http, HttpHandler, HttpResponse } from "msw";
 import response from "./resposneData.json";
 
+// import lineReview from "@constants/json/board/board_comments.json";
+
 import { isEmpty } from "lodash";
 
 interface bodyTypes {
@@ -145,6 +147,96 @@ const reviewHandler: HttpHandler[] = [
       );
     }
   ),
+
+  // 사용자가 작성한 한줄평 목록 조회
+  http.get(
+    `${import.meta.env.VITE_SERVER_URL}/api/v1/linereview/:nickname`,
+    ({ params, request }) => {
+      const authorization = request.headers.get("Authorization");
+      const { nickname } = params;
+
+      // Authorization 또는 nickname 보내지 않은 경우
+      if (!authorization || !nickname) {
+        return HttpResponse.json(
+          {
+            message:
+              "Authorization 값을 추가 또는 Path Validation으로 nickname 값을 추가했는지 확인해주세요.",
+            errorCode: "ERR_EMPTY_BODY_AUTH",
+          },
+          { status: 400, statusText: "Bad Request" }
+        );
+      }
+
+      const url = new URL(request.url);
+      const size = Number(url.searchParams.get("size")) || 10; // 데이터를 보내줄 개수 (기본값: 10개)
+      const lastReviewId = Number(url.searchParams.get("lastReviewId")) || null;
+      const lastCreatedAt =
+        Number(url.searchParams.get("lastCreatedAt")) || null;
+
+      // 현재 사용자가 작성한 한줄평 조회
+      // let filterLineReview =
+    }
+  ),
+
+  // 사용자가 작성한 한줄평 목록 조회
+  // ${import.meta.env.VITE_SERVER_URL}/api/v1/linereview/:nickname
+  // param = size: 가져올 데이터 개수, lastReviewId: 제일 마지막에 본 리뷰 ID, lastCreatedAt: 제일 마지막에 본 리뷰 Date
+  // headers -> 필수
+  /*
+  
+      {
+        "size": 10,
+        "content": [
+          {
+            "id": 0,
+            "writerNickname": "string",
+            "userId": 0,
+            "movieId": 0,
+            "rating": 0,
+            "context": "string",
+            "isSpoiler": true,
+            "likes": 0,
+            "dislikes": 0,
+            "createdAt": "2024-12-10T08:18:09.435Z"
+          }
+        ],
+        "number": 0, // 현재 페이지 번호
+        // 처리안해도 되는 데이터
+        "sort": {
+          "empty": true,
+          "sorted": true,
+          "unsorted": true
+        },
+        "numberOfElements": 0, // 현재 페이지의 항목 수
+        
+        // 페이지 정보
+        "pageable": {
+          "offset": 0,
+          "sort": {
+            "empty": true,
+            "sorted": true,
+            "unsorted": true
+          },
+          "paged": true,
+          "pageNumber": 0,
+          "pageSize": 0,
+          "unpaged": true
+        },
+
+        "first": true, // 첫 번째 페이지 여부
+        "last": true, // 마지막 페이지 여부
+        "empty": true // 데이터가 비어있는지 여부
+      }
+  
+  */
+
+  // 사용자가 작성한 한줄평 삭제
+  // delete!  /api/v1/linereview/{lineReviewId}
+  // isDelete = !isDelete
+
+  // 좋아요 / 싫어요
+  // 자기 글 안되고, 중복 불가능(좋아요 / 싫어요 취소), 좋아요 누르고 싫어요 누르면 자동으로 업데이트
+  // 보내야되는 값은  "lineReviewId": 1, "preference": "LIKE, DISLIKE"
 ];
 
 export default reviewHandler;
