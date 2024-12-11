4c6b1fb6cc8daf8b350899650371c9a63388c820
JGW-Korea <jgw6372@gmail.com>
2024-12-11 22:07:50 +0900

    fix: Header Config 수정 (#111)

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,42 +1,44 @@
-import { useHeaderConfig } from "@constants/header";
-import { Header, HeaderProps } from "@stories/header";
-import { NaviationProps } from "@type/navigation";
+import { HeaderConfigReturn, useHeaderConfig } from "@constants/header";
+import { isLogin } from "@recoil/atoms/isLoginState";
+import { Header } from "@stories/header";
+import { NaviationProps, HeaderProps } from "@type/navigation";
 import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
 
 // 전역에서 사용할 Header 컴포넌트
-function GlobalHeader({
-  show,
-  location,
-  navigate,
-}: // isLoginTestValue,
-NaviationProps) {
-  const [type, setType] = useState<HeaderProps["type"]>(undefined);
-  const [label, setLabel] = useState<HeaderProps["label"]>(undefined);
-  const [activeBtn, setActiveBtn] =
-    useState<HeaderProps["activeBtn"]>(undefined);
+function GlobalHeader({ headerType, label, location, navigate }: HeaderProps) {
+  const isLoginState = useRecoilValue(isLogin);
+  // const [label, setLabel] = useState<HeaderProps["label"]>(undefined);
+  const [activeBtn, setActiveBtn] = useState<HeaderConfigReturn>(undefined);
 
   // 현재 경로에 맞는 type, label, activeBtn 값 할당
   useEffect(() => {
     const { pathname } = location; // 현재 주소값 가져오기
     const userInfo = JSON.parse(sessionStorage.getItem("user") || "{}");
 
+    // console.log(headerType);
+
+    const headerType = useHeaderConfig(isLoginState.isLoginState, navigate);
+
+    console.log(headerType);
+    // console.log(headerType);
+    setActiveBtn(headerType);
+
+    // setActiveBtn(headerType.buttons); // 헤더 버튼 지정
+
+    // if (type === "") setType(undefined);
+    // else setType(headerType);
+
     // console.log(userInfo);
     // console.log(pathname);
 
-    // const headerType = useHeaderConfig(
-    //   pathname,
-    //   isLoginTestValue.state,
-    //   navigate
-    // );
-
     // setType(headerType.type); // 헤더 타입 지정
     // setLabel(headerType.label); // 헤더 라벨 지정
-    // setActiveBtn(headerType.buttons); // 헤더 버튼 지정
-  }, [location]);
+  }, [isLoginState, location]);
 
-  return null;
-
-  // return <Header type={type} label={label} activeBtn={activeBtn} />;
+  // 로그인과 주소에 따라서 헤더가 보이지 않아야 함
+  // 주소는 헤더 타입 정의할 때 해놓은 상태
+  return <Header type={headerType} label={label} activeBtn={activeBtn} />;
 }
 
 export default GlobalHeader;
