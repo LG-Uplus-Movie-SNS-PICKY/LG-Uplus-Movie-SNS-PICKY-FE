5a99589b593cb6d04fa4666ef06beb8b414a8db4
JGW-Korea <jgw6372@gmail.com>
2024-12-14 04:29:50 +0900

    fix: 출력 제거

--- src/pages/main/components/genres-movie/index.tsx
+++ src/pages/main/components/genres-movie/index.tsx
@@ -27,15 +27,14 @@ function GenresMovie() {
 
   // 다른 장르 버튼 클릭 시 해당 장르 영화 변경
   const GenreOnClick = (movieId: number) => {
-    console.log(movieId);
     setSelectButton(movieId);
   };
 
-  useEffect(() => {
-    if (!isLoading) {
-      console.log(genreMovies?.pages[0].data.content);
-    }
-  }, [isLoading]);
+  // useEffect(() => {
+  //   if (!isLoading) {
+  //     console.log(genreMovies?.pages[0].data.content);
+  //   }
+  // }, [isLoading]);
 
   return (
     <div css={styles.genreContainer()}>
