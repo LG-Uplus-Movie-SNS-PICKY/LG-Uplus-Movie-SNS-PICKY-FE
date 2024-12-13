0d1859fe66bb7e81f0810a7d0e5f32327b47da6f
JGW-Korea <jgw6372@gmail.com>
2024-12-13 21:47:25 +0900

    feat: 장르별 영화 조회 API 초기 설정 (#235)

--- src/api/movie/index.ts
+++ src/api/movie/index.ts
@@ -1,13 +1,19 @@
 import apiClient from "@api";
 
-// Top 10 Movie 조회 GET
+// Top 10 Movie 조회 GET API
 export async function fetchTopMovie() {
   const { data } = await apiClient.get("/movie/top10");
   return data;
 }
 
-// 사용자 추천 영화 조회 GET
+// 사용자 추천 영화 조회 GET API
 export async function fetchRecommendMovie() {
   const { data } = await apiClient.get("/movie/recommend");
+  return data;
+}
+
+// 장르별 영화 조회 GET API
+export async function fetchGenreMovie(genreId: number) {
+  const { data } = await apiClient.get(`/movie/genre?genreId=${genreId}`);
   return data;
 }
