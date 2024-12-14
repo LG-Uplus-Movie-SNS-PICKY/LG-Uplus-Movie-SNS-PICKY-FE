1ddfd656d5d63aa7dab8bac1fdda8867df19e6d3
JGW-Korea <jgw6372@gmail.com>
2024-12-14 21:14:45 +0900

    feat: Playlist Post API Mocking Object 설계 (#264)

--- src/mocks/handlers/playlist/index.ts
+++ src/mocks/handlers/playlist/index.ts
@@ -4,9 +4,68 @@ import movies from "@constants/json/movie/movie.json";
 import movieLikes from "@constants/json/movie/movieLikes.json";
 import playlists from "@constants/json/playlist/playlists.json";
 import playlistMovies from "@constants/json/playlist/playlistMovies.json";
+import { getCookie } from "@util/cookie";
+import { isElement, isEmpty } from "lodash";
+
+interface PlaylistPostBodyTypes {
+  movieId: number[];
+  title: string;
+}
 
 // Movie - Playlist 관련 모킹 API(Mocking Object) 설계
 const playlistHandler: HttpHandler[] = [
+  // 플레이리스트 추가 API (Mocking Object)
+  http.post(
+    `${import.meta.env.VITE_SERVER_URL}/api/v1/admin/playlist`,
+    async ({ request }) => {
+      const authorization = request.headers.get("Authorization");
+      const body = (await request.json()) as PlaylistPostBodyTypes;
+      const userInfo = getCookie("user") || {};
+
+      // Headers, body, userInfo(또는 권환이 어드민이 아닐 경우)가 없을 경우
+      if (
+        isElement(userInfo) ||
+        !userInfo.isAuthUser ||
+        isEmpty(body) ||
+        !authorization
+      ) {
+        return HttpResponse.json(
+          {
+            message:
+              "권한이 없습니다. Request Headers에 Authorization를 추가 또는 로그인(계정이 관리자인지 확인)을 하셨는지 또는 body를 보냈는지 확인해주세요.",
+          },
+          { status: 403 }
+        );
+      }
+
+      // 플레이리스트 추가
+      let newPlaylistId = playlists.length + 1;
+      playlists.push({
+        movie_playlist_id: newPlaylistId,
+        movie_playlist_title: body.title,
+      });
+
+      // 플레이리스트에 속한 영화 추가
+      body.movieId.forEach((movie) => {
+        playlistMovies.push({
+          playlist_movie_id: playlistMovies.length + 1,
+          movie_id: movie,
+          movie_playlist_id: newPlaylistId,
+        });
+      });
+
+      return HttpResponse.json({
+        code: 200,
+        message: "요청이 성공적으로 처리되었습니다.",
+        data: {
+          playlistId: 0,
+          title: "0",
+        },
+        success: true,
+      });
+    }
+  ),
+
   // 플레이리스트 조회 API(Mocking Object)
   http.get(
     `${import.meta.env.VITE_SERVER_URL}/api/v1/movie/playlist/all`,
