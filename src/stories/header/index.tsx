7dc89c240651670d1c6025f69e96f46093f96f44
JGW-Korea <jgw6372@gmail.com>
2024-12-18 05:58:11 +0900

    fix 전역 - 헤더 아이콘 수정 (#349)

--- src/stories/header/index.tsx
+++ src/stories/header/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./index.styles";
 
-import Logo from "@assets/icons/logo.svg?react";
+import Logo from "@assets/icons/main_logo.svg?react";
 import ArrowLeft from "@assets/icons/arrow_left.svg?react";
 import { useNavigate } from "react-router-dom";
 // import { useNavigate } from "react-router-dom";
